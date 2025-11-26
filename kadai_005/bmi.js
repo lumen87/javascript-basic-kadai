25a3f5a6903b958aebb018a16330be7fb10761a3
lumen87 <ymmt78mrk@gmail.com>
2025-11-26 22:20:07 +0900

    再提出：5章課題

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,9 +1,7 @@
 //変数の宣言・値の代入
 let bodyWeight = 68;
 let bodyHeight = 1.7;
-
-//定数の宣言
-const bmi = (bodyWeight / (bodyHeight * bodyHeight))
+let bmi = (bodyWeight / (bodyHeight * bodyHeight));
 
 //コンソールへの出力
 console.log(bmi);
