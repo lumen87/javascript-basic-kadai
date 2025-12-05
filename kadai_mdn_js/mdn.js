d8bcb0f07c928fd601037f451dd6b1794c48158c
lumen87 <ymmt78mrk@gmail.com>
2025-12-05 22:46:55 +0900

    12章課題を提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+//今日の日付を取得する関数を作成する
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+
+//今日の日付を出力する
+console.log(`${year}年${month}月${day}日`);
